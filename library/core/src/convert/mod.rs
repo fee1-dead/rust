adfd09e4c3a329ef222fb41b04ef97c90c6472cd
Deadbeef <ent3rm4n@gmail.com>
2021-07-08 21:00:36 +0800

    Document reflexive impl for From

--- library/core/src/convert/mod.rs
+++ library/core/src/convert/mod.rs
@@ -542,6 +542,9 @@ where
 // From (and thus Into) is reflexive
 #[stable(feature = "rust1", since = "1.0.0")]
 impl<T> From<T> for T {
+    /// Converts a `T` into a `T`.
+    ///
+    /// This function has no cost because it simply returns the value passed to it.
     fn from(t: T) -> T {
         t
     }
